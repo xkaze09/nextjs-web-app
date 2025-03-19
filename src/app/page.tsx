32309897db6c752f4745cbd6d132876475410239
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-03-20 02:39:02 +0800

    Add Greet and Counter components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import { Greet } from "./components/greet";
+import { Counter } from "./components/counter";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -12,6 +13,8 @@ export default function Home() {
           height={38}
           priority
         />
+        < Greet />
+        < Counter />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
