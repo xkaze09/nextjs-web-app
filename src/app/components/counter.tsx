587db09e84c5051171a8690032d3dca598c36596
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-03-20 02:39:10 +0800

    Add Counter component to the app

---
+++ src/app/components/counter.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { useState } from "react";
+
+export const Counter = () => {
+    const [ count, setCount ] = useState(0);
+
+    return (
+        <button onClick={() => setCount (count+1)}> Count {count} times </button>
+    );
+}
